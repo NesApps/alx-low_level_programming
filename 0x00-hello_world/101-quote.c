947854cbe1ba8de0443048c58a88d7d8ea408d51
Nestor <freemanchino333@gmail.com>
2023-02-18 04:35:37 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
 */
 int main(void)
 {
-putsf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+putf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
