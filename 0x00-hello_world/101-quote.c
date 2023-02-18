f99392a7fbd48ba89cfd6aef1b75478ae7da7a2d
Nestor <freemanchino333@gmail.com>
2023-02-18 04:39:03 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,12 @@
 *main - A C program that prints a line to the standard error
 *Return 1 (success)
 */
+
 int main(void)
+
 {
+
 putf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
 return (1);
 }
