2a43d114766a2081e464781f9e9b732d59fa8eb9
Nestor <freemanchino333@gmail.com>
2023-02-18 05:28:36 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,12 @@
 
 int main(void)
 {
-    const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    ssize_t len = sizeof(message) - 1;
-    ssize_t ret = write(STDERR_FILENO, message, len);
-
-    if (ret != len)
-    {
-        return 1;
-    }
-
-    return 0;
+const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+ssize_t len = sizeof(message) - 1;
+ssize_t ret = write(STDERR_FILENO, message, len);
+if (ret != len)
+{
+return 1;
+}
+return 0;
 }
