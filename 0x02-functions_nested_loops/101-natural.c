bf55167631fdf2cc817da505e55d32075238ba74
Nestor <freemanchino333@gmail.com>
2023-02-21 20:38:38 -0800

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,10 +10,11 @@ int main(void)
 {
 
 
-inti, sum = 0;
+int i, sum = 0;
+
 for (i = 0; i< 1024; i++)
 {
-if ((i % 3) == 0 || (i % 5) == 0)
+if ((i % 3 == 0) || (i % 5 == 0))
 sum += i;
 }
 printf("%d\n", sum);
