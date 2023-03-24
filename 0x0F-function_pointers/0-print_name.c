951e59411f2ae57c4027640881d90ed9f79562a1
Nestor <freemanchino333@gmail.com>
2023-03-24 11:01:17 -0700

    update

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -8,10 +8,14 @@
 * Return: nothing
 **/
 
-void print_name(char *name, void (*f)(char *))
-{
-if (name == NULL || f == NULL)
-return;
 
-f(name);
+void print_with_hello(char *name) {
+    printf("Hello, %s!\n", name);
 }
+
+int main() {
+    char *name = "Alice";
+    print_name(name, &print_with_hello); // prints "Hello, Alice!"
+    return 0;
+}
+
